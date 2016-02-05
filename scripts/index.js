3c1872d8dcf326616368185da9de911ee111fdd6
hipra <gyorgy.bokros@kinja.com>
2016-02-05 16:52:32 +0100

    Color changing on collision
    The ball’s color is changing on collision. I also added CSS with some
    basic rules

--- scripts/index.js
+++ scripts/index.js
@@ -5,7 +5,7 @@ var x = 20;
 var y = 20;
 var vX = 7;
 var vY = 3;
-var ballSize = 50;
+var ballSize = 10;
 
 var wallLeft = 0;
 var wallRight = context.canvas.width - ballSize;
@@ -13,22 +13,28 @@ var wallRight = context.canvas.width - ballSize;
 var wallTop = 0;
 var wallBottom = context.canvas.height - ballSize;
 
+var colors = ["#00B8A9", "#EB586F", "#4AA0D5", "#F8C957"];
+
 function drawBall() {
 	context.clearRect(0, 0, context.canvas.width, context.canvas.height);
 	context.fillRect(x, y, ballSize, ballSize);
 	
 	if (x + vX >= wallRight) {
         handleCollisionX(wallRight);
+        context.fillStyle = colors[0];
 	} else if (x + vX <= wallLeft) {
         handleCollisionX(wallLeft);
+        context.fillStyle = colors[1];
 	} else {
 		x = x + vX;
 	}
 
 	if (y + vY >= wallBottom) {
         handleCollisionY(wallBottom);
+        context.fillStyle = colors[2];
 	} else if (y + vY <= wallTop) {
         handleCollisionY(wallTop);
+        context.fillStyle = colors[3];
 	} else {
 		y = y + vY;
 	}
